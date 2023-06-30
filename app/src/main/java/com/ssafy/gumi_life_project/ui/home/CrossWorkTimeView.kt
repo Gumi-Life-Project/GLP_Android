f5d3d7317b38c9c4203b680ea5f338e37e0472ae
noion0511 <noion0511@gmail.com>
2023-06-30 15:41:08 +0900

    feat : 메인 화면 횡단보도 커스텀 UI 기능
    - 횡단 보도 화면 내부의 시간을 표시하는 화면 코드
    - 디자인 화면 구현을 위한 border 추가
    - 커스텀 UI 생성을 위한 설명 attrs
    
    #3

---
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/home/CrossWorkTimeView.kt
@@ -0,0 +1,29 @@
+package com.ssafy.gumi_life_project.ui.home
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.LinearLayout
+import com.ssafy.gumi_life_project.R
+import com.ssafy.gumi_life_project.databinding.ViewCrossworkTimeBinding
+
+class CrossWorkTimeView @JvmOverloads constructor(
+    context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : LinearLayout(context, attrs, defStyleAttr) {
+
+    private val binding: ViewCrossworkTimeBinding = ViewCrossworkTimeBinding.inflate(
+        LayoutInflater.from(context),
+        this,
+        true
+    )
+
+    init {
+        val typedArray = context.obtainStyledAttributes(attrs, R.styleable.CrossWorkTimeView)
+        val timeText = typedArray.getString(R.styleable.CrossWorkTimeView_timeText)
+        typedArray.recycle()
+
+        binding.apply {
+            textviewTime.text = timeText ?: "00:00"
+        }
+    }
+}
