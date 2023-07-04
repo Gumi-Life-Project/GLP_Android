68d77394470187892342d1dea98c0b950f3ce084
noion0511 <noion0511@gmail.com>
2023-07-05 01:25:27 +0900

    feat : 횡단보도 기능 구현에 필요한 데이터 선언
    - LightTime : 현재 신호등 색, 다음 신호까지 남은 시간
    - TriggerTime : 초록색으로 변하는 시간
    - Color : 신호등 색의 종류
    - SignalLight : 신호등 구분자
    
    #3

--- app/src/main/java/com/ssafy/gumi_life_project/data/model/CrossWalk.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/data/model/CrossWalk.kt
@@ -4,8 +4,53 @@ import com.ssafy.gumi_life_project.R
 
 data class CrossWalk(val time: String)
 
-enum class SignalLight(val titleResId: Int, val contentResId: Int) {
-    SIGNAL_LIGHT_1(R.string.cross_walk_title1, R.string.cross_walk_explain1),
-    SIGNAL_LIGHT_2(R.string.cross_walk_title2, R.string.cross_walk_explain2),
-    SIGNAL_LIGHT_3(R.string.cross_walk_title3, R.string.cross_walk_explain3)
+//currentColor 현재 신호등 색, remainingTime 다음 신호등 색까지 남은 시간
+data class LightTime(val currentColor: Color, val remainingTime: Long) {
+    val remainingTimeString: String
+        get() {
+            val minutes = remainingTime / 60
+            val seconds = remainingTime % 60
+            return String.format("%02d:%02d", minutes, seconds)
+        }
+}
+
+data class TriggerTime(
+    val year: Int,
+    val month: Int,
+    val day: Int,
+    val hour: Int,
+    val minute: Int,
+    val second: Int
+)
+
+enum class Color {
+    GREEN,
+    RED
+}
+
+enum class SignalLight(
+    val titleResId: Int,
+    val contentResId: Int,
+    private val greenDuration: Int,
+    private val redDuration: Int
+) {
+    SIGNAL_LIGHT_1(R.string.cross_walk_title1, R.string.cross_walk_explain1, 23, 98),
+    SIGNAL_LIGHT_2(R.string.cross_walk_title2, R.string.cross_walk_explain2, 24, 106),
+    SIGNAL_LIGHT_3(R.string.cross_walk_title3, R.string.cross_walk_explain3, 17, 143);
+
+    fun calculateRemainingTime(triggerTimeMillis: Long): LightTime {
+        val currentTimeMillis = System.currentTimeMillis()
+        val elapsedTimeMillis = currentTimeMillis - triggerTimeMillis
+        val remainingTimeMillis = elapsedTimeMillis % ((greenDuration + redDuration) * 1000)
+
+        return if (remainingTimeMillis > greenDuration * 1000) {
+            LightTime(
+                Color.RED,
+                (redDuration * 1000 - (remainingTimeMillis - greenDuration * 1000)) / 1000
+            )
+        } else {
+            LightTime(Color.GREEN, (greenDuration * 1000 - remainingTimeMillis) / 1000)
+        }
+    }
+
 }
