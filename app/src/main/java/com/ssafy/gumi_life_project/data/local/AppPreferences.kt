65cbe7d22d8017cd4cc11473ab2b33b4b29bea0f
DoYeon <dodoyeon0425@gmail.com>
2023-07-04 23:33:13 +0900

    feat : 앱 최초 실행 여부를 저장하고 확인하는 코드 추가
    -앱 최초 실행시 셔틀 버스 정보 초기화를 하기 위해 최초 실행인지 실행 상태를 업데이트 하는 Shared preference추가
    -앱 최초 실행 시 Shared preference에 이후 실행이 최초 실행이 아님을 표시하는 함수를 AppPreference에 추가
    -앱 실행 후 최초 실행인지 아닌지 여부를 판단하고 최초 실행이라면 실행 상태를 업데이트하는 분기문을 App 클래스에 추가
    #5

--- app/src/main/java/com/ssafy/gumi_life_project/data/local/AppPreferences.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/data/local/AppPreferences.kt
@@ -2,11 +2,29 @@ package com.ssafy.gumi_life_project.data.local
 
 import android.content.Context
 import android.content.SharedPreferences
+import com.google.gson.GsonBuilder
 
 object AppPreferences {
     private const val LOGIN_SESSION = "login.session"
+    private const val SHUTTLE_BUS_INFO_PREFERENCE = "shuttle_bus_info_preference"
+
+    const val APP_RUN_STATE = "app_run_state"
+
+    private lateinit var preferences: SharedPreferences
 
     fun openSharedPrep(context: Context): SharedPreferences {
         return context.getSharedPreferences(LOGIN_SESSION, Context.MODE_PRIVATE)
     }
+
+    fun openShuttleBusSharedPreference(context: Context): SharedPreferences {
+        preferences =
+            context.getSharedPreferences(SHUTTLE_BUS_INFO_PREFERENCE, Context.MODE_PRIVATE)
+        return preferences
+    }
+
+    fun updateAppRunState() {//앱 최초 실행이 아님을 표시
+        preferences.edit().putBoolean(APP_RUN_STATE, false)
+    }
+
+
 }
