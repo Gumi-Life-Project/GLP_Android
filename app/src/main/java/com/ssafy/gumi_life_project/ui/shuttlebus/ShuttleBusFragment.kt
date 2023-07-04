11df2dce048a65effc810f4e6d822ee67e8f0618
DoYeon <dodoyeon0425@gmail.com>
2023-07-05 00:09:37 +0900

    feat : 셔틀버스 노선, 셔틀버스 정류장 정보를 화면에 보여주는 기능 추가
    -SharedPreference에 있는 셔틀버스 정보를 ShuttleBusViewModel에서 불러오고 관리하도록 ShuttleBusViewModel수정
    -ShuttleBusViewModel수정에 따라 화면개발을 위해 임시로 보여줬던 데이터를 지우고 ShuttleBusViewModel에 있는 데이터를 화면에 보여주도록 ShuttleBusFragment수정
    #5

--- app/src/main/java/com/ssafy/gumi_life_project/ui/shuttlebus/ShuttleBusFragment.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/shuttlebus/ShuttleBusFragment.kt
@@ -8,7 +8,6 @@ import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.RecyclerView
 import com.ssafy.gumi_life_project.R
 import com.ssafy.gumi_life_project.data.model.ShuttleBusLine
-import com.ssafy.gumi_life_project.data.model.ShuttleBusStop
 import com.ssafy.gumi_life_project.databinding.FragmentShuttleBusBinding
 import com.ssafy.gumi_life_project.util.template.BaseFragment
 import dagger.hilt.android.AndroidEntryPoint
@@ -19,6 +18,7 @@ class ShuttleBusFragment : BaseFragment<FragmentShuttleBusBinding>(
 ) {
     private val viewModel by viewModels<ShuttleBusViewModel>()
     private lateinit var adapter: ShuttleBusLineAdapter
+    private lateinit var busLineList: MutableList<ShuttleBusLine>
 
     override fun onCreateBinding(
         inflater: LayoutInflater,
@@ -30,28 +30,19 @@ class ShuttleBusFragment : BaseFragment<FragmentShuttleBusBinding>(
     }
 
     override fun init() {
-        binding?.viewModel = viewModel
+        bindingNonNull.viewModel = viewModel
+        initData()
         initRecyclerView()
+        initObserver()
+    }
+
+    fun initData() {
+        viewModel.getShuttleBusLineList()
     }
 
     private fun initRecyclerView() {
-        val list_line = mutableListOf<ShuttleBusLine>()
-
-        val list_stop = mutableListOf<ShuttleBusStop>()
-        list_stop.add(ShuttleBusStop("구미1", 1.1, 1.1, "07:10", false))
-        list_stop.add(ShuttleBusStop("구미2", 1.1, 1.1, "07:25", true))
-        list_stop.add(ShuttleBusStop("구미3", 1.1, 1.1, "07:40", false))
-        list_stop.add(ShuttleBusStop("구미4", 1.1, 1.1, "07:50", false))
-        list_line.add(ShuttleBusLine("구미 노선", list_stop))
-
-        val list_stop2 = mutableListOf<ShuttleBusStop>()
-        list_stop2.add(ShuttleBusStop("칠곡1", 1.1, 1.1, "07:10", false))
-        list_stop2.add(ShuttleBusStop("칠곡2", 1.1, 1.1, "07:25", false))
-        list_stop2.add(ShuttleBusStop("칠곡3", 1.1, 1.1, "07:40", false))
-        list_stop2.add(ShuttleBusStop("칠곡4", 1.1, 1.1, "07:50", false))
-        list_line.add(ShuttleBusLine("칠곡 노선", list_stop2))
-
-        adapter = ShuttleBusLineAdapter(list_line, childFragmentManager)
+        busLineList = mutableListOf<ShuttleBusLine>()
+        adapter = ShuttleBusLineAdapter(busLineList, childFragmentManager)
         adapter.itemClickListener = object : ShuttleBusLineAdapter.ItemClickListener {
             override fun onExpandButtonClicked(
                 isItemOpen: Boolean,
@@ -70,5 +61,13 @@ class ShuttleBusFragment : BaseFragment<FragmentShuttleBusBinding>(
 
         }
         binding?.recyclerviewShuttleBusLine?.adapter = adapter
+    }
+
+    private fun initObserver() {
+        viewModel.shuttleBusLineList.observe(viewLifecycleOwner) {
+            busLineList.clear()
+            busLineList.addAll(it)
+            adapter.notifyDataSetChanged()
+        }
     }
 }
--- app/src/main/java/com/ssafy/gumi_life_project/ui/shuttlebus/ShuttleBusViewModel.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/shuttlebus/ShuttleBusViewModel.kt
@@ -1,7 +1,21 @@
 package com.ssafy.gumi_life_project.ui.shuttlebus
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.ssafy.gumi_life_project.data.local.AppPreferences
+import com.ssafy.gumi_life_project.data.model.ShuttleBusLine
 import com.ssafy.gumi_life_project.util.template.BaseViewModel
 
 class ShuttleBusViewModel : BaseViewModel() {
+    private val _shuttleBusLineList: MutableLiveData<MutableList<ShuttleBusLine>> =
+        MutableLiveData()
+    val shuttleBusLineList: LiveData<MutableList<ShuttleBusLine>>
+        get() = _shuttleBusLineList
+
+    fun getShuttleBusLineList() {
+        val lineList = AppPreferences.getShuttleBusInfo()
+        _shuttleBusLineList.postValue(lineList)
+    }
+
 
 }
