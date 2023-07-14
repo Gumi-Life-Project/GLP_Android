130b7b59551909cf04889833b656c1ca52ea2bfe
noion0511 <noion0511@gmail.com>
2023-07-14 23:53:03 +0900

    feat : 상세화면 코틀린 파일 생성
    - 충돌 해결
    - 상세화면 코틀린 파일 생성
    #24

---
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/board/BoardDetailFragment.kt
@@ -0,0 +1,71 @@
+package com.ssafy.gumi_life_project.ui.board
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import com.ssafy.gumi_life_project.R
+import com.ssafy.gumi_life_project.databinding.FragmentBoardDetailBinding
+import com.ssafy.gumi_life_project.ui.main.LoadingDialog
+import com.ssafy.gumi_life_project.util.template.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class BoardDetailFragment : BaseFragment<FragmentBoardDetailBinding>(
+    R.layout.fragment_board_detail
+) {
+    private val viewModel by viewModels<BoardViewModel>()
+    private lateinit var adapter: BoardListAdapter
+
+    override fun onCreateBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentBoardDetailBinding {
+        return FragmentBoardDetailBinding.inflate(inflater, container, false).apply {
+            lifecycleOwner = viewLifecycleOwner
+            viewModel = this@BoardDetailFragment.viewModel
+        }
+    }
+
+    override fun init() {
+        initToolbar()
+        initRecyclerView()
+        initObserver()
+
+    }
+
+    private fun initToolbar() {
+        bindingNonNull.toolbar.toolbarBackButtonTitle.text =
+            resources.getString(R.string.board_title)
+        bindingNonNull.toolbar.buttonGoBack.setOnClickListener {
+            findNavController().navigate(R.id.action_boardListFragment_to_homeFragment)
+        }
+    }
+
+    private fun initRecyclerView() {
+
+    }
+
+    private fun initObserver() {
+        with(viewModel) {
+            board.observe(viewLifecycleOwner) {
+                adapter.setBoardList(it)
+            }
+
+            errorMsg.observe(viewLifecycleOwner) { event ->
+                event.getContentIfNotHandled()?.let {
+                    showToast(it)
+                }
+            }
+
+            val dialog = LoadingDialog(requireContext())
+            isLoading.observe(viewLifecycleOwner) {
+                if (isLoading.value!!) {
+                    dialog.show()
+                } else if (!isLoading.value!!) {
+                    dialog.dismiss()
+                }
+            }
+        }
+    }
+}
