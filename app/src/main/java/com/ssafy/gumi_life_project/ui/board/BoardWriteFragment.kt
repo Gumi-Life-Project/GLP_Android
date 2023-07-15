b6061bde358b2fd48df2284aca8e1739a44abf0c
DoYeon <dodoyeon0425@gmail.com>
2023-07-15 18:39:44 +0900

    feat : 게시글 작성 화면 툴바 제목, 뒤로가기 추가
    -게시글 작성 화면의 툴바 제목 추가
    -게시글 작성 화면의 뒤로가기 버튼 클릭 시 게시글 리스트 화면으로 이동하도록 수정
    #27

--- app/src/main/java/com/ssafy/gumi_life_project/ui/board/BoardWriteFragment.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/board/BoardWriteFragment.kt
@@ -2,6 +2,7 @@ package com.ssafy.gumi_life_project.ui.board
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ssafy.gumi_life_project.R
 import com.ssafy.gumi_life_project.databinding.FragmentBoardWriteBinding
 import com.ssafy.gumi_life_project.util.template.BaseFragment
@@ -21,7 +22,19 @@ class BoardWriteFragment : BaseFragment<FragmentBoardWriteBinding>(
     }
 
     override fun init() {
-
+        initToolbar()
+        initListener()
     }
 
+    private fun initToolbar() {
+        bindingNonNull.toolbarBoardWrite.toolbarBackButtonTitle.text = resources.getString(R.string.board_write_toolbar_title)
+        bindingNonNull.toolbarBoardWrite.toolbarBackButton.setOnClickListener {
+            findNavController().navigate(R.id.action_shuttleBusFragment_to_homeFragment)
+        }
+    }
+    private fun initListener() {
+        bindingNonNull.toolbarBoardWrite.buttonGoBack.setOnClickListener {
+            findNavController().navigate(R.id.action_boardWriteFragment_to_boardListFragment)
+        }
+    }
 }
