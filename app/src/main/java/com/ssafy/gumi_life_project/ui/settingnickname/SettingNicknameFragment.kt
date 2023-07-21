f85addba2ef8b303f91ef702f90abbb3dfecafda
jinyoung <kimjinzer099@gmail.com>
2023-07-21 20:43:17 +0900

    feat : 닉네임 설정 화면 툴바 추가 - 닉네임 설정 화면 레이아웃 수정 - 위 화면에서 필요한 strings.xml 추가 #20

--- app/src/main/java/com/ssafy/gumi_life_project/ui/settingnickname/SettingNicknameFragment.kt
+++ app/src/main/java/com/ssafy/gumi_life_project/ui/settingnickname/SettingNicknameFragment.kt
@@ -1,21 +1,46 @@
 package com.ssafy.gumi_life_project.ui.settingnickname
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.ssafy.gumi_life_project.R
 import com.ssafy.gumi_life_project.databinding.FragmentSettingNicknameBinding
+import com.ssafy.gumi_life_project.ui.login.LoginViewModel
 import com.ssafy.gumi_life_project.util.template.BaseFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+private const val TAG = "SettingNicknameFragment_구미"
+@AndroidEntryPoint
 class SettingNicknameFragment : BaseFragment<FragmentSettingNicknameBinding>(
     R.layout.fragment_setting_nickname
 ) {
+    private val viewModel by viewModels<SettingNicknameViewModel>()
     override fun onCreateBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
     ): FragmentSettingNicknameBinding {
-        return FragmentSettingNicknameBinding.inflate(inflater, container, false)
+        return FragmentSettingNicknameBinding.inflate(inflater, container, false).apply {
+            viewModel = this@SettingNicknameFragment.viewModel
+        }
     }
 
     override fun init() {
+        initToolBar()
+        initListener()
+    }
+
+    private fun initToolBar() {
+        bindingNonNull.toolbarSettingNickname.toolBarTitle.text = resources.getString(R.string.setting_nickname_title)
+    }
+
+    private fun initListener() {
+        bindingNonNull.buttonNicknameSetting.setOnClickListener {
+            val nickName = bindingNonNull.edittextInputNickname.text.toString()
+            viewModel.makeNickName(nickName)
+            Log.d(TAG, "initListener: $nickName")
+            findNavController().navigate(R.id.action_settingNicknameFragment_to_homeFragment)
+        }
     }
 }
